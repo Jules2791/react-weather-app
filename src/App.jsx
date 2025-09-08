c57e9f02cd9ecb809910dcff2966e192d0580ab0
Jules2791 <may.julie@yahoo.co.uk>
2025-09-08 19:07:05 +0100

    Added Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,26 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/148259-julie-may"
-          target="_blank"
-        >
-          Julie May
-        </a>{" "}
-        and is open-sourced on
-        <a
-          href="https://github.com/Jules2791/react-weather-app"
-          target="_blank"
-        >
-          {" "}
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/148259-julie-may"
+            target="_blank"
+          >
+            Julie May
+          </a>{" "}
+          and is open-sourced on
+          <a
+            href="https://github.com/Jules2791/react-weather-app"
+            target="_blank"
+          >
+            {" "}
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
