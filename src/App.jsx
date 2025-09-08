98f1c684ef49ae69ea328891eb9558c7aafcf41c
Jules2791 <may.julie@yahoo.co.uk>
2025-09-08 18:56:01 +0100

    Added footer and links

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.shecodes.io/graduates/148259-julie-may"
+          target="_blank"
+        >
+          Julie May
+        </a>{" "}
+        and is open-sourced on
+        <a
+          href="https://github.com/Jules2791/react-weather-app"
+          target="_blank"
+        >
+          {" "}
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
